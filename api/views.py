b863f1e111d6e33c2e3f57ddd720a999bc6b07b0
Arturo Elizalde <aemasia@gmail.com>
2022-04-27 02:55:25 +0200

    correcting distinc

--- api/views.py
+++ api/views.py
@@ -8,7 +8,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 class Ip2AsnViewSet(viewsets.ModelViewSet):
     # TODO: distinc query fields
     http_method_names = ['get']
-    queryset = Ip2Asn.objects.all().distinct('as_number', 'as_description', 'country_code')
+    queryset = Ip2Asn.objects.all()
     serializer_class = Ip2AsnSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['id', 'as_number']
